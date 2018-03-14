0b22b62b1f27a4bf8c732244fcf5c9e9f071523a
misterfox81 <lspezia81@gmail.com>
2018-03-14 13:09:12 +0100

    Prova da eclipse

---
+++ Lezione1-JavaBase/src/ProvaPrimoProgramma.java
@@ -0,0 +1,11 @@
+
+public class ProvaPrimoProgramma {
+
+	// Questa sotto è la parte fondamentale di ogni programma!
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ciao a tutti questo è il mio primo programma");
+		
+	}
+
+}
